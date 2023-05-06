fc88f13a0e1708bd69874289f36aee2a9f3355cf
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-05-06 11:54:55 -0500

    feat: Added message item

---
+++ techlinkerappmobile/lib/widgets/message_item.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import '../models/developer_unique_item.dart';
+
+class MessageItem extends StatelessWidget {
+  final DeveloperUniqueItem item;
+  const MessageItem({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 13),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), color: primaryColor),
+      child: ListTile(
+        onTap: () => {print("Clicked")},
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(30),
+          child: Image.network(
+            item.image!,
+            height: 64,
+            width: 56,
+            fit: BoxFit.cover,
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.name!,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 18, color: textColor),
+            ),
+            SizedBox(height: 5),
+            Text(
+              "${item.name!} Developer",
+              style: const TextStyle(
+                color: textColor,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Date',
+              style: TextStyle(
+                color: textColor,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
+            CircleAvatar(
+              backgroundColor: warningColor,
+              radius: 12,
+              child: Text('2', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
