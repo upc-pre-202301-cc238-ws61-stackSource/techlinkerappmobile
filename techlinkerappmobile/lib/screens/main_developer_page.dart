6a2f0bde7afefc117147e8c05861400cfa4a3d5a
BrancoVillegas <89035920+BrancoVillegas@users.noreply.github.com>
2023-05-06 14:41:32 -0500

    feat: Added home developer section

---
+++ techlinkerappmobile/lib/screens/main_developer_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+import '../screens/home_developer.dart';
+
+class MainDeveloperPage extends StatefulWidget {
+  const MainDeveloperPage({super.key});
+
+  @override
+  State<MainDeveloperPage> createState() => _MainDeveloperPageState();
+}
+
+class _MainDeveloperPageState extends State<MainDeveloperPage>{
+  int index = 0;
+
+  final screens = [
+    DeveloperHome(),
+    Center(child: Text("Mail2", style: TextStyle(color: Colors.green))),
+    Center(
+        child: Text("Mail3",
+            style: TextStyle(color: Color.fromRGBO(233, 30, 99, 1)))),
+    Center(child: Text("Mail4", style: TextStyle(color: Colors.blue))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: secondaryColor,
+          iconTheme: MaterialStateProperty.all(
+            const IconThemeData(color: Colors.white),
+          ),
+          labelTextStyle: MaterialStateProperty.all(
+            TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        child: NavigationBar(
+          height: 90,
+          backgroundColor: buttonColor,
+          selectedIndex: index,
+          onDestinationSelected: (index) {
+            setState(() {
+              this.index = index;
+            });
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home_filled), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.notifications_none_outlined),
+                label: 'notifications'),
+            NavigationDestination(
+                icon: Icon(Icons.messenger_outline), label: 'Messages'),
+            NavigationDestination(
+                icon: Icon(Icons.person_2_outlined), label: 'Dev'),
+          ],
+        ),
+      ),
+    );
+  }
+}
