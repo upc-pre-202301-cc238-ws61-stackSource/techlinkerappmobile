e1e8fffb79634c6420a2ca708c46f2e374394f12
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2023-06-29 23:36:06 -0500

    Notification permission

--- techlinkerappmobile/lib/main.dart
+++ techlinkerappmobile/lib/main.dart
@@ -20,10 +20,26 @@ void main() async {
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   runApp(const MyApp());
 }
-void requestNotificationPermission() async {
-  final PermissionStatus status = await Permission.notification.request();
-  print('Permiso de notificación: $status');
+Future<void> requestNotificationPermission() async {
+  PermissionStatus status = await Permission.notification.request();
+  if (status.isDenied) {
+    // El usuario ha denegado los permisos de notificación
+    print('El usuario ha denegado los permisos de notificación');
+    // Puedes mostrar un mensaje o realizar alguna acción adicional aquí
+  } else if (status.isPermanentlyDenied) {
+    // El usuario ha denegado permanentemente los permisos de notificación
+    print('El usuario ha denegado permanentemente los permisos de notificación');
+    // Puedes mostrar un mensaje y guiar al usuario a la configuración de la aplicación para habilitar los permisos manualmente
+    openAppSettings();
+  } else if (status.isGranted) {
+    // Los permisos de notificación han sido concedidos
+    print('Los permisos de notificación han sido concedidos');
+    // Puedes continuar con la lógica de tu aplicación relacionada con las notificaciones aquí
+    // Por ejemplo, enviar una notificación de prueba
+  }
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
